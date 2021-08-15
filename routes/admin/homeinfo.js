38a6ea27b9e0df936cb6f300e8cf3bc66dab3a9e
VoonQiaoQing <qqvoon@gmail.com>
2021-08-15 23:28:41 +0800

    countdown releases DONE

--- routes/admin/homeinfo.js
+++ routes/admin/homeinfo.js
@@ -14,7 +14,6 @@ router.get("/", home_page);
 router.get("/home", async function (req, res) {
 	return res.redirect("/");
 });
-router.get("/choosereleasetable-data", choosereleasetable_data);
 
 router.get("/edithomedes", edithomedescription_page);
 router.post("/edithomedes", edithomedescription_process);
@@ -26,75 +25,6 @@ router.post("/edithomeimagepolicy",
 	]),
 	edithomeimagepolicy_process);
 
-
-
-
-
-
-/**
- * Provide Bootstrap table with data
- * @param {import('express').Request}  req 
- * @param {import('express').Response} res 
- */
-// ---------------- 
-//	TODO:	Common URL paths here
-async function choosereleasetable_data(req, res) {
-	try {
-		let pageSize = parseInt(req.query.limit);    //(req.query.pageSize)? req.query.pageSize : 10;
-		let offset = parseInt(req.query.offset);   //page * pageSize;
-		let sortBy = (req.query.sort) ? req.query.sort : "dateCreated";
-		let sortOrder = (req.query.order) ? req.query.order : "asc";
-		let search = req.query.search;
-
-		//if (page < 0)     throw new HttpError(400, "Invalid page number");
-		if (pageSize < 0) throw new HttpError(400, "Invalid page size");
-		if (offset < 0) throw new HttpError(400, "Invalid offset index");
-
-		// TODO: Do your search filter with this
-		/** @type {import('sequelize/types').WhereOptions} */
-		const conditions = (search) ? {
-			[Op.or]: {
-				"dateCreated": { [Op.substring]: search },
-				"dateUpdated": { [Op.substring]: search },
-				"moviereleasedate": { [Op.substring]: search },
-				"movieenddate": { [Op.substring]: search },
-				"moviename": { [Op.substring]: search },
-				"movieagerating": { [Op.substring]: search },
-				"movieduration": { [Op.substring]: search },
-				"moviegenre": { [Op.substring]: search },
-				"movie_uuid": { [Op.substring]: req.body.movie_uuid }
-			}
-		} : undefined;
-
-		const total = await ModelMovieInfo.count({ where: conditions });
-		const pageTotal = Math.ceil(total / pageSize);
-
-		const pageContents = await ModelMovieInfo.findAll({
-			offset: offset,
-			limit: pageSize,
-			order: [[sortBy, sortOrder.toUpperCase()]],
-			where: conditions,
-			raw: true	//	Data only, model excluded
-		});
-
-		// const choosemovies = await ModelMovies.findAll({raw: true});
-		return res.json({
-			"total": total,
-			"rows": pageContents
-		});
-	}
-	catch (error) {
-		console.error(`Request query errors`);
-		console.error(error);
-		// return next(new HttpError(400, error.message));
-	}
-}
-
-
-
-
-
-
 /**
  * Renders the home page
  * @param {Request}  req Express Request handle
@@ -104,73 +34,134 @@ async function choosereleasetable_data(req, res) {
 //	TODO:	Common URL paths here
 async function home_page(req, res) {
 
+	class Release {
+		constructor(newly, countdown, image) {
+			this._newly = newly;
+			this._countdown = countdown;
+			this._image = image;
+		}
+		get newly() { return this._newly; }
+		set newly(newly) { this._newly = newly; }
+
+		get countdown() { return this._countdown; }
+		set countdown(countdown) { this._countdown = countdown; }
+
+		get image() { return this._image; }
+		set image(image) { this._image = image; }
+	};
+
 	const homeinfo = await ModelHomeInfo.findOne({
 		where: {
 			"homeinfo_uuid": "test"
 		}
 	});
 
-	// const movieinfo = await ModelMovieInfo.findAll({
-	// 	// where: {
-	// 	// 	"admin_uuid": '4ea99629-8760-44b6-95f8-762a4b1f8a87'
-	// 	// }
-	// });
+	const movieinfo = await ModelMovieInfo.findAll();
 
-	// const diff = {};
+	const Newest = []; //Display Newly Created, Display Countdown and Image
 
-	// const Newest = [];
-	// const Length = movieinfo.length;
+	for (let i = 0; i < movieinfo.length; i++) {
+		const countDownDate = new Date(movieinfo[i].moviereleasedate).getTime();
+		var now = new Date().getTime();
+		var distance1 = countDownDate - now;
 
-	// // for (let i = 0; i < Length; i++) {
-	// // 	findnewestimage.push(movieinfo[i].movieimage);
-	// // }
+		const countDownCreation = new Date(movieinfo[i].dateCreated).getTime();
+		var now = new Date().getTime();
+		var distance2 = countDownCreation - now;
 
-	// for (let i = 0; i < Length; i++) {
-	// 	const countDownDate = new Date(movieinfo[i].dateCreated).getTime();
-	// 	var now = new Date().getTime();
-	// 	var distance = countDownDate - now;
+		const release = new Release(distance2, distance1, movieinfo[i].movieimage);
+		Newest.push(release);
+	}
 
-	// 	// Newest.push(distance);
-	// 	diff[distance] = movieinfo[i].movieimage;
-	// }
+	var countdown1 = "Coming Soon...";
+	var countdown2 = "Coming Soon...";
+	var countdown3 = "Coming Soon...";
+	var countdown4 = "Coming Soon...";
+	var release_img1 = "No-Image-PlaceHolder.png";
+	var release_img2 = "No-Image-PlaceHolder.png";
+	var release_img3 = "No-Image-PlaceHolder.png";
+	var release_img4 = "No-Image-PlaceHolder.png";
 
-	// for (var key in diff) {
-	// 	Newest.push(key);
-	// }
-	
-	// const ComingSoon = "Comgin "
-	// const NewestSorted = Newest.sort(); //Milliseconds ASC
-	
-	// if (NewestSorted.length == 0) {
-	// 	NewestSorted.push("Coming Soon!")
-	// }
-	// if (NewestSorted.length == 1) {
-	// 	NewestSorted.push("Coming Soon!")
-	// }
-	// if (NewestSorted.length == 2) {
-	// 	NewestSorted.push("Coming Soon!")
-	// }
-	// if (NewestSorted.length == 3) {
-	// 	NewestSorted.push("Coming Soon!")
-	// }
+	const NewestAgain = Newest.sort();
 
-	// for (var key in diff) {
-	// 	if (key == Newest[-1]) {
-	// 		One = diff[0];
-	// 	}
-	// }	
+	if (NewestAgain.length != 0) {
 
-	// logout, just render index.handlebars
-
+		if (NewestAgain.length == 1) {
+			countdown1 = "Out Now!";
+			if (NewestAgain[0]._countdown > 0) {
+				countdown1 = NewestAgain[0]._countdown;
+			}
+			release_img1 = NewestAgain[0]._image;
+		}
+		if (NewestAgain.length == 2) {
+			countdown1 = "Out Now!";
+			countdown2 = "Out Now!";
+			if (NewestAgain[0]._countdown > 0) {
+				countdown1 = NewestAgain[0]._countdown;
+				countdown2 = NewestAgain[1]._countdown;
+			}
+			release_img1 = NewestAgain[0]._image;
+			release_img2 = NewestAgain[1]._image;
+		}
+		if (NewestAgain.length == 3) {
+			if (NewestAgain[0]._countdown > 0) {
+				countdown1 = NewestAgain[0]._countdown;
+				countdown2 = NewestAgain[1]._countdown;
+				countdown3 = NewestAgain[2]._countdown;
+			}
+			else {
+				countdown1 = "Out Now!";
+				countdown2 = "Out Now!";
+				countdown3 = "Out Now!";
+			}
+			release_img1 = NewestAgain[0]._image;
+			release_img2 = NewestAgain[1]._image;
+			release_img3 = NewestAgain[2]._image;
+		}
+		else {
+			if (NewestAgain[0]._countdown > 0) {
+				countdown1 = NewestAgain[0]._countdown;
+			}
+			else {
+				countdown1 = "Out Now!";
+			}
+			if (NewestAgain[1]._countdown > 0) {
+				countdown2 = NewestAgain[1]._countdown;
+			}
+			else {
+				countdown2 = "Out Now!";
+			}
+			if (NewestAgain[2]._countdown > 0) {
+				countdown3 = NewestAgain[2]._countdown;
+			}
+			else {
+				countdown3 = "Out Now!";
+			}
+			if (NewestAgain[3]._countdown > 0) {
+				countdown4 = NewestAgain[3]._countdown;
+			}
+			else {
+				countdown4 = NewestAgain[3]._countdown;
+			}
+			release_img1 = NewestAgain[3]._image;
+			release_img2 = NewestAgain[2]._image;
+			release_img3 = NewestAgain[1]._image;
+			release_img4 = NewestAgain[0]._image;
+		}
+	}
 	return res.render('home', {
 		homedescription: homeinfo.homedescription,
 		homepolicy: homeinfo.homepolicy,
 		homeimage: homeinfo.homeimage,
-		homepolicyimage: homeinfo.homepolicyimage
-		// release_name1: NewestSorted[0],
-		// release_name2: NewestSorted[-2],
-		// release_name3: NewestSorted[-3],
-		// release_name4: NewestSorted[-4]
+		homepolicyimage: homeinfo.homepolicyimage,
+		release_img1: release_img1,
+		release_img2: release_img2,
+		release_img3: release_img3,
+		release_img4: release_img4,
+		countdown1: countdown1,
+		countdown2: countdown2,
+		countdown3: countdown3,
+		countdown4: countdown4
 	});
 }
 
@@ -308,87 +299,3 @@ async function edithomeimagepolicy_process(req, res, next) {
 		});
 	}
 }
-
-// /**
-//  * Renders the edithomebestreleases page
-//  * @param {Request}  req Express Request handle
-//  * @param {Response} res Express Response handle
-//  */
-// // ---------------- 
-// //	TODO:	Common URL paths here
-// async function edithomebestreleases_page(req, res) {
-// 	console.log("Home Best Releases page accessed");
-// 	return res.render('editbestreleases', {
-
-// 	});
-// };
-
-// /**
-//  * Renders the login page
-//  * @param {Request}  req Express Request handle
-//  * @param {Response} res Express Response handle
-//  */
-// async function edithomebestreleases_process(req, res) {
-// 	try {
-// 		const homebestreleases = await ModelBestReleases.create({
-// 			"email": req.body.email,
-// 			"homeid": req.body.homeid,
-// 			"release_image1": req.body.release_image1,
-// 			"release_name1": req.body.release_name1,
-// 			"release_image2": req.body.release_image2,
-// 			"release_name2": req.body.release_name2,
-// 			"release_image3": req.body.release_image3,
-// 			"release_name3": req.body.release_name3,
-// 			"release_image4": req.body.release_image4,
-// 			"release_name4": req.body.release_name4
-// 		});
-// 		console.log('Description created: $(homebestreleases.email)');
-// 	}
-// 	catch (error) {
-// 		console.error(`Credentials problem: ${req.body.email}`);
-// 		console.error(error);
-// 		return res.render('/edithomeimagepolicy', { errors: errors });
-// 	}
-// }
-
-// /**
-//  * Renders the edithomebestreleases page
-//  * @param {Request}  req Express Request handle
-//  * @param {Response} res Express Response handle
-//  */
-// // ---------------- 
-// //	TODO:	Common URL paths here
-// async function edithomebestreleases_page(req, res) {
-// 	console.log("Home Best Releases page accessed");
-// 	return res.render('editbestreleases', {
-
-// 	});
-// };
-
-// /**
-//  * Renders the login page
-//  * @param {Request}  req Express Request handle
-//  * @param {Response} res Express Response handle
-//  */
-// async function edithomebestreleases_process(req, res) {
-// 	try {
-// 		const homebestreleases = await ModelBestReleases.create({
-// 			"email": req.body.email,
-// 			"homeid": req.body.homeid,
-// 			"release_image1": req.body.release_image1,
-// 			"release_name1": req.body.release_name1,
-// 			"release_image2": req.body.release_image2,
-// 			"release_name2": req.body.release_name2,
-// 			"release_image3": req.body.release_image3,
-// 			"release_name3": req.body.release_name3,
-// 			"release_image4": req.body.release_image4,
-// 			"release_name4": req.body.release_name4
-// 		});
-// 		console.log('Description created: $(homebestreleases.email)');
-// 	}
-// 	catch (error) {
-// 		console.error(`Credentials problem: ${req.body.email}`);
-// 		console.error(error);
-// 		return res.render('/edithomeimagepolicy', { errors: errors });
-// 	}
-// }
